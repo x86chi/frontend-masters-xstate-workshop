1882a8a02c5697187aff191ce2ada6b6a84c574b
Muhun Kim <iam@muhun.kim>
2020-07-01 16:53:01 +0900

    Exercise 1 complete
    Create a state machine

--- src/01/index.js
+++ src/01/index.js
@@ -1,25 +1,29 @@
 const elBox = document.querySelector('#box');
 
+const model = {
+  initial: 'active',
+  state: {
+    active: { on: { CLICK: 'inactive' } },
+    inactive: { on: { CLICK: 'active' } },
+  },
+};
+
 // Pure function that returns the next state,
 // given the current state and sent event
 function transition(state, event) {
-  switch (
-    state
-    // Add your state/event transitions here
-    // to determine and return the next state
-  ) {
-  }
+  return model.state[state].on[event] || state;
 }
 
 // Keep track of your current state
-let currentState = undefined;
+let currentState = model.initial;
 
 function send(event) {
   // Determine the next value of `currentState`
-
+  const nextState = transition(currentState, event);
+  currentState = nextState;
   elBox.dataset.state = currentState;
 }
 
 elBox.addEventListener('click', () => {
-  // send a click event
+  send('CLICK');
 });
